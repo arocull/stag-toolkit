8d229aa60a51da5c66dbada41f0c0eec5cdde023
Alan O'Cull <masteralan2001@gmail.com>
2024-12-14 02:35:01 -0500

    fix: ci install script?

--- godot-build/install.sh
+++ godot-build/install.sh
@@ -10,7 +10,7 @@ sudo mkdir -p $INSTALLDIR
 sudo chown $USER:users $INSTALLDIR
 
 GODOT_URL="https://github.com/godotengine/godot-builds/releases/download/${GODOT_VERSION}/Godot_v${GODOT_VERSION}_linux.x86_64.zip"
-GODOT_BIN="/usr/local/bin/Godot_v${GODOT_VERSION}_linux.x86_64"
+GODOT_BIN="/usr/local/threed/Godot_v${GODOT_VERSION}_linux.x86_64"
 
 if [ ! -f $GODOT_BIN ]; then
     wget --waitretry=0.5 --tries=100 -O /tmp/godot.zip $GODOT_URL && sudo unzip -o /tmp/godot.zip -d $INSTALLDIR && rm -rf /tmp/godot.zip && ln -s -f $GODOT_BIN $BINDIR/godot
