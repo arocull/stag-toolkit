8010b144d72273c4d0d01d92ec6e300fdaa6554e
Alan O'Cull <masteralan2001@gmail.com>
2024-12-14 01:30:23 -0500

    ci for Abyss

---
+++ godot-build/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo "Installing ${GODOT_VERSION}"
+
+BINDIR="/usr/local/bin"
+INSTALLDIR="/usr/local/threed"
+
+sudo mkdir -p $INSTALLDIR
+sudo chown $USER:users $INSTALLDIR
+
+GODOT_URL="https://github.com/godotengine/godot-builds/releases/download/${GODOT_VERSION}/Godot_v${GODOT_VERSION}_linux.x86_64.zip"
+GODOT_BIN="/usr/local/bin/Godot_v${GODOT_VERSION}_linux.x86_64"
+
+if [ ! -f $GODOT_BIN ]; then
+    wget --no-verbose --waitretry=0.5 --tries=100 -O /tmp/godot.zip $GODOT_URL && sudo unzip -o /tmp/godot.zip -d $INSTALLDIR && rm -rf /tmp/godot.zip && ln -s -f $GODOT_BIN $BINDIR/godot
+fi
+
+sudo chmod a-w /usr/local/bin
+echo "ThreeD: finished Godot installation"
