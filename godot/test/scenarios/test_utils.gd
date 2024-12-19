3fc9749029b9b5e889423519f6084058a28c763e
Alan O'Cull <masteralan2001@gmail.com>
2024-12-19 15:39:49 -0500

    test: add combination test

--- godot/test/scenarios/test_utils.gd
+++ godot/test/scenarios/test_utils.gd
@@ -11,3 +11,4 @@ func _ready():
 	StagTest.assert_equal(6, StagUtils.factorial(3), "3! = 3 * 2 * 1 = 6")
 	StagTest.assert_equal(120, StagUtils.factorial(5), "5! = 5 * 4 * 3 * 2 * 1 = 20 * 6 = 120")
 	StagTest.assert_equal(620448401733239409999872.0, StagUtils.factorial(24), "24! = very large number")
+	StagTest.assert_equal(2024, StagUtils.factorial(24) / (StagUtils.factorial(3) * StagUtils.factorial(24-3)), "nCr(24, 3)")
